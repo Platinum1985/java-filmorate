29947651773cd22ba00b40d004db4862620a1563
Ishxan <ishmsu2@inbox.ru>
2026-01-21 00:49:25 +0300

    попытка 8

--- src/main/java/ru/yandex/practicum/homeTheatre/model/User.java
+++ src/main/java/ru/yandex/practicum/homeTheatre/model/User.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.homeTheatre.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.HashSet;
